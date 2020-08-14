70f630f4e6246982791f89a7d9ce86594593d181
mhmulford0 <mhmulford0@gmail.com>
2020-08-14 10:26:15 -0500

    styled comps added, character comp started

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { ThemeProvider } from 'styled-components'
+import styled from 'styled-components';
+
+import Character from './components/Character'
+
 import './App.css';
+import theme from "./Theme";
+
+const StlyedApp = styled.div`
+  text-align: center;
+  h1 {
+    color: white;
+  }
+`;
+
+
+
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <StlyedApp>
+        <div className="App">
+          <h1>Characters</h1>
+          <Character />
+        </div>
+      </StlyedApp>
+    </ThemeProvider>
   );
 }
 
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,11 @@
-// Write your Character component here
+import React from 'react'
+
+function Character() {
+  return (
+    <div>
+      <h1>Test</h1>
+    </div>
+  )
+}
+
+export default Character
