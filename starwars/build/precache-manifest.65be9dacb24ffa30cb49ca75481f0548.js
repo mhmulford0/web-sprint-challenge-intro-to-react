627fdff9cd36ed07136a3f2d607f35a2e581e38e
mhmulford0 <mhmulford0@gmail.com>
2020-08-17 12:17:12 -0500

    build

---
+++ starwars/build/precache-manifest.65be9dacb24ffa30cb49ca75481f0548.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95cb105ef43073353863eefbfd053900",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e6274e8dd65dddc9446",
+    "url": "/static/css/main.40ed244b.chunk.css"
+  },
+  {
+    "revision": "08c7faa4bab21d69772f",
+    "url": "/static/js/2.9b4b6818.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9b4b6818.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e6274e8dd65dddc9446",
+    "url": "/static/js/main.f30ddd7c.chunk.js"
+  },
+  {
+    "revision": "a2ce235db025746ced47",
+    "url": "/static/js/runtime-main.42cd862b.js"
+  },
+  {
+    "revision": "29d4e1070c864ac3a78d885f0b9cc2bf",
+    "url": "/static/media/bg.29d4e107.png"
+  }
+]);
